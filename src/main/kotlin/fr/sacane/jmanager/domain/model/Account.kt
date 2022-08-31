75a5aa12d65f62e57f4d1a2e483c7c283a54ec25
M20079 <johan.ramaroson@departement77.fr>
2022-09-01 01:17:27 +0200

    [FIX] - fixed business logic of accounts

--- src/main/kotlin/fr/sacane/jmanager/domain/model/Account.kt
+++ src/main/kotlin/fr/sacane/jmanager/domain/model/Account.kt
@@ -1,8 +1,33 @@
 package fr.sacane.jmanager.domain.model
 
 
-data class Account(
-        val amount: Double,
-        val labelAccount: String,
-        val sheets: List<Sheet>
-)
+
+class Account(
+        private var amount: Double,
+        private var labelAccount: String,
+        private var sheets: List<Sheet>
+){
+
+    fun sheets(): List<Sheet>{
+        return sheets.distinct()
+    }
+
+    fun label(): String{
+        return labelAccount
+    }
+    operator fun Account.plusAssign(earned: Double){
+        this.amount += earned
+    }
+    operator fun Account.minusAssign(loss: Double){
+        amount -= loss
+    }
+    fun transaction(delta: Double, otherAccount: Account, isEntry: Boolean){
+        if(isEntry){
+            this -= delta
+            otherAccount += delta
+        } else {
+            this += delta
+            otherAccount -= delta
+        }
+    }
+}
