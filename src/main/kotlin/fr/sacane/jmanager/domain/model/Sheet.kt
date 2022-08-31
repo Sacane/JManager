d61b7f9cbe57c77a2a46c71004d418abf1388bf3
M20079 <johan.ramaroson@departement77.fr>
2022-08-31 22:18:24 +0200

    [FEAT] - implements Sheet with his ID

--- src/main/kotlin/fr/sacane/jmanager/domain/model/Sheet.kt
+++ src/main/kotlin/fr/sacane/jmanager/domain/model/Sheet.kt
@@ -3,6 +3,7 @@ package fr.sacane.jmanager.domain.model
 import java.time.LocalDate
 
 data class Sheet(
+        var id: SheetId,
         var label: String,
         var date: LocalDate,
         var value: Double,
---
+++ src/main/kotlin/fr/sacane/jmanager/domain/model/SheetId.kt
@@ -0,0 +1,5 @@
+package fr.sacane.jmanager.domain.model
+
+data class SheetId(val id: Long){
+    fun get(): Long = id
+}
