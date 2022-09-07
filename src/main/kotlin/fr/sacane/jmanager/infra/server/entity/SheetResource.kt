5cda72b7d24ab063f1b69be2f357e1f450e43db7
M20079 <johan.ramaroson@departement77.fr>
2022-09-07 14:22:06 +0200

    [FIX] - move fields to constructor directly

--- src/main/kotlin/fr/sacane/jmanager/infra/server/entity/SheetResource.kt
+++ src/main/kotlin/fr/sacane/jmanager/infra/server/entity/SheetResource.kt
@@ -10,23 +10,18 @@ import javax.persistence.Table
 
 @Entity
 @Table(name="sheet")
-class SheetResource {
-
+class SheetResource(
     @Id
     @GeneratedValue
     @Column(unique = true, name = "id_sheet", nullable = false)
-    var idSheet: Long? = null
-
+    var idSheet: Long? = null,
     @Column(unique = true, name = "label_sheet")
-    var label: String? = null
-
+    var label: String? = null,
     @Column(name= "amount")
-    var amount: Double? = null
-
+    var amount: Double? = null,
     @Column(name="date")
-    var date: LocalDate? = null
-
+    var date: LocalDate? = null,
     @Column(name="isEntry")
     var isEntry: Boolean? = null
 
-}
+)
