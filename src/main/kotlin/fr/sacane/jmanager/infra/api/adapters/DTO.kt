7d4bc6473d8870f200a0acc028cd273244cf840c
Sacane <johan.ramaroson@gmail.com>
2022-09-08 10:38:11 +0200

    [FEAT] - Add DTO implementation

--- src/main/kotlin/fr/sacane/jmanager/infra/api/adapters/DTO.kt
+++ src/main/kotlin/fr/sacane/jmanager/infra/api/adapters/DTO.kt
@@ -1,5 +1,8 @@
 package fr.sacane.jmanager.infra.api.adapters
 
+import fr.sacane.jmanager.domain.model.Sheet
+import java.time.LocalDate
+
 data class UserDTO(
     val id: Long,
     val username: String,
@@ -11,4 +14,23 @@ data class UserDTO(
 data class UserPasswordDTO(
     val username: String,
     val password: String
+)
+
+data class SheetDTO(
+    val label: String,
+    val amount: Double,
+    val action: String,
+    val account: String,
+    val date: LocalDate
+)
+
+data class AccountDTO(
+    val amount: Double,
+    val labelAccount: String,
+    val sheets: List<SheetDTO>
+)
+
+data class UserSheetDTO(
+    val userDTO: UserDTO
+
 )
--- src/main/kotlin/fr/sacane/jmanager/infra/api/adapters/controllers/JManagerController.kt
+++ src/main/kotlin/fr/sacane/jmanager/infra/api/adapters/controllers/JManagerController.kt
@@ -1,12 +1,9 @@
 package fr.sacane.jmanager.infra.api.adapters.controllers
 
-import fr.sacane.jmanager.domain.model.UserId
-import fr.sacane.jmanager.domain.port.apiside.ApiPort
 import fr.sacane.jmanager.infra.api.adapters.ApiAdapter
 import fr.sacane.jmanager.infra.api.adapters.UserDTO
 import fr.sacane.jmanager.infra.api.adapters.UserPasswordDTO
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
