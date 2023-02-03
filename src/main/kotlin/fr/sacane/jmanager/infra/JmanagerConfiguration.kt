c33998e2274a64eeca97c34f901dd7afb56d4fe0
Sacane <johan.ramaroson@gmail.com>
2023-02-04 00:13:27 +0100

    remove useless adapter

--- src/main/kotlin/fr/sacane/jmanager/infra/JmanagerConfiguration.kt
+++ src/main/kotlin/fr/sacane/jmanager/infra/JmanagerConfiguration.kt
@@ -1,9 +1,10 @@
 package fr.sacane.jmanager.infra
 
 import fr.sacane.jmanager.domain.port.apiside.TransactionReader
-import fr.sacane.jmanager.domain.port.apiside.UserRegisterFlow
+import fr.sacane.jmanager.domain.port.apiside.UserRegister
 import fr.sacane.jmanager.domain.adapter.TransactionReaderAdapter
-import fr.sacane.jmanager.domain.adapter.UserRegisterFlowAdapter
+import fr.sacane.jmanager.domain.adapter.UserRegisterAdapter
+import fr.sacane.jmanager.domain.port.serverside.LoginTransactor
 import fr.sacane.jmanager.domain.port.serverside.TransactionRegister
 import fr.sacane.jmanager.domain.port.serverside.UserTransaction
 import fr.sacane.jmanager.infra.api.TransactionValidator
@@ -22,18 +23,13 @@ class JmanagerConfiguration {
     }
 
     @Bean
-    fun portUser(userTransaction: UserTransaction) : UserRegisterFlow{
-        return UserRegisterFlowAdapter(userTransaction)
+    fun portUser(userTransaction: UserTransaction, loginTransaction: LoginTransactor) : UserRegisterAdapter{
+        return UserRegisterAdapter(userTransaction, loginTransaction)
     }
 
     @Bean
     fun apiAdapter(apiPort: TransactionReader, userPort: UserTransaction): TransactionValidator {
         return TransactionValidator(apiPort, userPort)
-    }
-
-    @Bean
-    fun userApiAdapter(adapter: UserRegisterFlow): UserControlAdapter{
-        return UserControlAdapter(adapter)
     }
 
 }
