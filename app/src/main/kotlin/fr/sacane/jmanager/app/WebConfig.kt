436cbf82f45d7afce3bffc3d09a3d25a3487f35e
Sacane <johan.ramaroson@gmail.com>
2023-02-07 15:48:45 +0100

    delete useless dependencies

--- app/src/main/kotlin/fr/sacane/jmanager/app/WebConfig.kt
+++ app/src/main/kotlin/fr/sacane/jmanager/app/WebConfig.kt
@@ -1,9 +1,5 @@
 package fr.sacane.jmanager.app
 
-import fr.sacane.jmanager.server.adapters.LoginTransactionAdapter
-import fr.sacane.jmanager.server.repositories.UserRepository
-import fr.sacane.jmanager.server.spring.SpringLayerService
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
 
