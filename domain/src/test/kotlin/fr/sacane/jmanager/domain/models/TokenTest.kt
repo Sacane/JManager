37b50c8762202012f414a032fc4b1fe91471e922
Sacane <johan.ramaroson@gmail.com>
2023-02-08 18:03:14 +0100

    add more test for token

--- domain/src/test/kotlin/fr/sacane/jmanager/domain/models/TokenTest.kt
+++ domain/src/test/kotlin/fr/sacane/jmanager/domain/models/TokenTest.kt
@@ -3,12 +3,22 @@ package fr.sacane.jmanager.domain.models
 
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.time.LocalDateTime
 import java.util.*
 
 class TokenTest {
+
     @Test
-    fun `token check identity should be effective for same token`(){
+    fun `token should not be able to check its expiration when last refresh is not specified`(){
+        val token = Token(UUID.randomUUID(), null, UUID.randomUUID())
+        assertThrows<IllegalArgumentException> {
+            token.isExpired()
+        }
+    }
+
+    @Test
+    fun `Ticket check identity should be effective for same token`(){
         val token = Token(UUID.randomUUID(), null, UUID.randomUUID())
         val user = User(UserId(0), "test", "test@gmail.com", "test", mutableListOf(), Password("test"), mutableListOf())
         val ticket = Ticket(user, token)
