bae8df5be314814dda1732ecfa9d0e94cfe2c989
Sacane <johan.ramaroson@gmail.com>
2023-02-08 17:39:34 +0100

    add token Test

---
+++ domain/src/test/kotlin/fr/sacane/jmanager/domain/models/TokenTest.kt
@@ -0,0 +1,28 @@
+package fr.sacane.jmanager.domain.models
+
+
+import kotlinx.coroutines.delay
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+import java.util.*
+import kotlin.concurrent.schedule
+
+class TokenTest {
+    @Test
+    fun `token check identity should be effective for same token`(){
+        val token = Token(UUID.randomUUID(), null, UUID.randomUUID())
+        val user = User(UserId(0), "test", "test@gmail.com", "test", mutableListOf(), Password("test"), mutableListOf())
+        val ticket = Ticket(user, token)
+        val verifiedToken = token.copy()
+        assertThat(ticket.checkForIdentity(verifiedToken)).isNotNull
+    }
+
+    @Test
+    fun `Token knows when it's expired or not`(){
+        val token = Token(UUID.randomUUID(), LocalDateTime.now().plusSeconds(5), UUID.randomUUID())
+        assertThat(token.isExpired()).isFalse
+        Thread.sleep(5000)
+        assertThat(token.isExpired()).isTrue
+    }
+}
