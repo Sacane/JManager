878686ee28b97b3f5dde2e79892bb63ac545939f
Sacane <johan.ramaroson@gmail.com>
2023-02-11 15:31:18 +0100

    Fixed configuration, by ordered injections

--- infra/src/main/kotlin/fr/sacane/jmanager/JmanagerConfiguration.kt
+++ infra/src/main/kotlin/fr/sacane/jmanager/JmanagerConfiguration.kt
@@ -1,12 +1,12 @@
 package fr.sacane.jmanager
 
-import fr.sacane.jmanager.domain.port.api.BudgetResolver
-import fr.sacane.jmanager.domain.port.api.LoginManager
-import fr.sacane.jmanager.domain.port.api.BudgetResolverApply
-import fr.sacane.jmanager.domain.port.api.UserRegister
+import fr.sacane.jmanager.domain.port.api.*
 import fr.sacane.jmanager.domain.port.spi.LoginInventory
 import fr.sacane.jmanager.domain.port.spi.TransactionRegister
 import fr.sacane.jmanager.domain.port.spi.UserTransaction
+import fr.sacane.jmanager.server.adapters.LoginTransactionAdapter
+import fr.sacane.jmanager.server.adapters.ServerTransactionAdapter
+import fr.sacane.jmanager.server.adapters.ServerUserAdapter
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -17,18 +17,28 @@ class JmanagerConfiguration {
 
 
     @Bean
+    fun userTransaction(): UserTransaction{
+        return ServerUserAdapter()
+    }
+    @Bean
+    fun serverAdapter(): TransactionRegister{
+        return ServerTransactionAdapter()
+    }
+    @Bean
+    fun loginTransactionInventory(): LoginInventory{
+        return LoginTransactionAdapter()
+    }
+    @Bean
     fun transactionReaderAdapter(serverAdapter: TransactionRegister, userTransaction: UserTransaction): BudgetResolver {
         return BudgetResolverApply(serverAdapter, userTransaction)
     }
-
     @Bean
-    fun loginManager(loginTransaction: LoginInventory) : LoginManager {
-        return LoginManager(loginTransaction)
+    fun loginManager(loginTransaction: LoginInventory, userTransaction: UserTransaction) : Administrator {
+        return LoginManager(loginTransaction, userTransaction)
     }
     @Bean
     fun userRegister(userTransaction: UserTransaction): UserRegister {
         return UserRegister(userTransaction)
     }
-
 
 }
