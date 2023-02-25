29a3e1c86e4414147b5feb6cdbe0ccbda518fd82
Sacane <johan.ramaroson@gmail.com>
2023-02-25 03:05:18 +0100

    add main page first style + header

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,11 +1,13 @@
 <template>
-  <header>
-    <nav>
-      goto
-    </nav>
+  <header class="bg-gray-200 py-4 border-b">
+    <h1 class="text-2xl font-bold text-center">
+      Bienvenue sur JManager
+    </h1>
   </header>
-  <slot />
+  <div class="mt-3xl">
+    <slot />
+  </div>
   <footer class="absolute bottom-0">
-    footer
+    @copyright 2023
   </footer>
 </template>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,3 +1,8 @@
 <template>
-  <h1>JManager</h1>
+  <div>
+    <h1 class="text-3xl font-bold text-center max-w-2xl">
+      Un sommaire rapide et pratique pour organiser votre budget et vos dépenses
+    </h1>
+    <span class="123">123</span>
+  </div>
 </template>
