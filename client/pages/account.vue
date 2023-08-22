32afd05b27262e67f342ef796023100140f216d5
Sacane <johan.ramaroson@gmail.com>
2023-08-22 17:58:37 +0200

    [FEAT] Datatable's design ok

--- client/pages/account.vue
+++ client/pages/account.vue
@@ -1,45 +1,36 @@
-import useAccounts from '../composables/useAccounts';
-import { AccountDTO } from '../types/index';
 <script setup lang="ts">
+import useAccounts from '../composables/useAccounts'
+
 definePageMeta({
   layout: 'sidebar-layout',
 })
 
-const {accounts, fetch} = useAccounts()
-const isAccountFilled = reactive({ok:false})
+const { accounts, fetch } = useAccounts()
+const isAccountFilled = reactive({ ok: false })
 
 const toAdd = () => {
   navigateTo('/addAccount')
 }
 
 onMounted(async () => {
-  await fetch();
-  isAccountFilled.ok = true
-  console.log(accounts.value)
+  await fetch()
+  isAccountFilled.ok = accounts.value.length > 0
 })
-
-const formatCurrency = (value: string) => {
-  const formattedValue = parseFloat(value).toFixed(2);
-  return `${formattedValue} €`;
-};
-
 </script>
 
 <template>
   <div w-full h-full flex>
-    <div v-if="isAccountFilled.ok">
+    <div v-if="isAccountFilled.ok" class=" bg-#f0f0f0 p20px">
       <PDataTable :value="accounts" table-style="min-width: 50rem">
         <PColumn field="labelAccount" header="Libellé du compte" :body-style="{ textAlign: 'center' }" :header-style="{ textAlign: 'center' }" />
-        <PColumn field="amount" header="Montant" :body-style="{ textAlign: 'center' }" :header-style="{ textAlign: 'center' }"/>
+        <PColumn field="amount" header="Montant actuel" :body-style="{ textAlign: 'center' }" :header-style="{ textAlign: 'center' }" />
       </PDataTable>
     </div>
     <div v-else min-w-500px>
       Ce profil n'a pas encore de compte enregistrés
     </div>
-    <div class="w-200px" mt-2>
-      <PButton label="Ajouter un nouveau compte" @click="toAdd" class="bg-purple"/>
+    <div class="w-full mt-2 flex items-center" >
+      <PButton label="Ajouter un nouveau compte" class="bg-purple w-250px" @click="toAdd" />
     </div>
-    
   </div>
-
 </template>
