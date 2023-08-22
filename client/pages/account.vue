130517e04e72498445e21bf156bb03f586fc2edd
Sacane <johan.ramaroson@gmail.com>
2023-08-22 18:04:34 +0200

    [FEAT] Datatable's design ok with good format

--- client/pages/account.vue
+++ client/pages/account.vue
@@ -4,24 +4,32 @@ import useAccounts from '../composables/useAccounts'
 definePageMeta({
   layout: 'sidebar-layout',
 })
-
+interface AccountFormatted{
+  labelAccount: string,
+  amount: string
+}
 const { accounts, fetch } = useAccounts()
 const isAccountFilled = reactive({ ok: false })
-
+const accountFormatted = ref<AccountFormatted[]>([])
 const toAdd = () => {
   navigateTo('/addAccount')
 }
-
 onMounted(async () => {
   await fetch()
   isAccountFilled.ok = accounts.value.length > 0
+  accountFormatted.value = accounts.value.map(account => {
+    return {
+      labelAccount: account.labelAccount,
+      amount: `${account.amount} €`,
+    };
+  });
 })
 </script>
 
 <template>
   <div w-full h-full flex>
     <div v-if="isAccountFilled.ok" class=" bg-#f0f0f0 p20px">
-      <PDataTable :value="accounts" table-style="min-width: 50rem">
+      <PDataTable :value="accountFormatted" table-style="min-width: 50rem">
         <PColumn field="labelAccount" header="Libellé du compte" :body-style="{ textAlign: 'center' }" :header-style="{ textAlign: 'center' }" />
         <PColumn field="amount" header="Montant actuel" :body-style="{ textAlign: 'center' }" :header-style="{ textAlign: 'center' }" />
       </PDataTable>
