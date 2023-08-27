6f1c5225c770036c906b0a32b1a4156c65cf0b0e
Sacane <johan.ramaroson@gmail.com>
2023-08-27 15:36:03 +0200

    add new page and composables dor date and sheets

---
+++ client/composables/useSheets.ts
@@ -0,0 +1,42 @@
+import useQuery from './useQuery';
+import { SheetDTO } from '../types/index';
+
+interface UserSheetDTO {
+  userId: number,
+  month: string,
+  year: number,
+  accountLabel: string
+}
+
+export default function useSheet() {
+  const {deleteQuery, post} = useQuery()
+  const sheets = ref<SheetDTO[]>([])
+  const {user} = useAuth()
+
+  async function findByDate(month: string, year: number, accountLabel: string) {
+    const response = post('sheets/get', {
+      userId: user.value?.id,
+      month: month,
+      year: year,
+      accountLabel: accountLabel
+    })
+    return response
+  }
+  
+  function saveSheet(accountLabel: string, sheetDTO: SheetDTO): Promise<SheetDTO> {
+    return post('/sheet/save', {
+      userId: user.value?.id,
+      accountLabel: accountLabel,
+      sheetDTO: sheetDTO
+    })
+  }
+
+  function deleteSheet(accountId: number, ids: Array<number>) : Promise<any>{
+    return deleteQuery('/sheet/delete', {
+      accountId: accountId,
+      sheetIds: ids
+    })
+  }
+
+  return {findByDate, saveSheet, deleteSheet}
+}
