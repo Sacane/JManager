a0df9e9bb4be55896c9e1832f53ae2f58fea5b8b
Sacane <johan.ramaroson@gmail.com>
2023-09-17 16:40:26 +0200

    rework of login page

--- client/pages/about.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-definePageMeta({
-  layout: 'foo',
-})
-</script>
-
-<template>
-  <h1>About</h1>
-</template>
--- client/pages/login.vue
+++ client/pages/login.vue
@@ -1,51 +1,70 @@
 <script setup lang="ts">
-import type { UserAuth } from '@/composables/useAuth'
+import { ref } from 'vue';
+import useAuth from '../composables/useAuth';
 
-definePageMeta({
-  layout: 'default',
-})
+const { login } = useAuth();
 
-const { login } = useAuth()
-
-const userAuth = reactive<UserAuth>({
+const userAuth = ref({
   username: '',
   password: '',
-})
+});
 </script>
 
+<style scoped>
+
+/* Styles personnalisés */
+.input {
+  @apply p-2 border border-gray-300 rounded-lg;
+}
+
+.btn-primary {
+  @apply bg-purple-600 text-white py-2 px-4 rounded-md transition hover:bg-purple-700;
+}
+
+.form-container{
+  background-color: white;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1); /* Ajoutez l'ombre ici */
+}
+</style>
+
 <template>
-  <div class="w-full h-auto md:h-full flex flex-col md:flex-row justify-center md:justify-evenly items-center">
-    <div class="flex flex-col w-full md:w-30% p-10 rounded-lg border border-gray-400">
-      <h1 class="text-3xl font-bold text-center">
-        Se connecter
-      </h1>
-      <form class="flex flex-col gap-2 p-3 border-rd-20px border-none">
-        <label for="username">Nom d'utilisateur</label>
-        <input id="username" v-model="userAuth.username" type="text" class="border border-gray-400 rounded-lg p-1 ">
-        <label for="password">Password</label>
-        <input v-model="userAuth.password" type="password" class="border border-gray-400 rounded-lg mb-4 p-1">
-        <button type="button" class="bg-#7F52FF text-white p-7px rounded-2" @click="login(userAuth)">
-          Login
-        </button>
-      </form>
-    </div>
-    <div class="flex flex-col w-full md:w30% p-10 rounded-lg border border-gray-400">
-      <h1 class="text-3xl font-bold text-center">
-        S'enregistrer
-      </h1>
-      <form class="flex flex-col gap-2 p-10 border-rd-20px border-none">
-        <label for="name">Nom d'utilisateur</label>
-        <input id="name" type="text" class="border border-gray-400 rounded-lg p-0.5">
-        <label for="email">Email</label>
-        <input id="email" type="email" class="border border-gray-400 rounded-lg p-0.5">
-        <label for="password">Mot de passe</label>
-        <input type="password" class="border border-gray-400 rounded-lg p-0.5">
-        <label for="confirm">Confirmer le mot de passe</label>
-        <input id="password" type="password" class="border border-gray-400 rounded-lg mb-4 p-0.5">
-        <button class="bg-#7F52FF text-white p-7px rounded-2">
+  <div class="flex-col items-center w-full h-full mt2">
+    <h1 class="text-4xl font-bold text-purple-600 mb-8">JManager application</h1>
+    <div class="w-full h-auto md:h-full flex flex-col md:flex-row justify-center md:justify-evenly items-center">
+      <!-- Connexion -->
+      <div class="flex flex-col w-full md:w-1/3 p-6 rounded-lg border border-gray-400 form-container">
+        <h1 class="text-3xl font-bold text-purple-600 text-center mb-6">
+          Se connecter
+        </h1>
+        <form class="flex flex-col gap-4">
+          <label for="username" class="text-sm font-medium text-gray-700">Nom d'utilisateur</label>
+          <input id="username" v-model="userAuth.username" type="text" class="input" />
+          <label for="password" class="text-sm font-medium text-gray-700">Mot de passe</label>
+          <input v-model="userAuth.password" type="password" class="input" />
+          <button type="button" class="btn-primary" @click="login(userAuth)">Login</button>
+        </form>
+      </div>
+
+      <!-- Enregistrement -->
+      <div class="flex flex-col w-full md:w-1/3 p-6 rounded-lg border border-gray-400 mt-6 md:mt-0 form-container">
+        <h1 class="text-3xl font-bold text-purple-600 text-center mb-6">
           S'enregistrer
-        </button>
-      </form>
+        </h1>
+        <form class="flex flex-col gap-4">
+          <label for="name" class="text-sm font-medium text-gray-700">Nom d'utilisateur</label>
+          <input id="name" type="text" class="input" />
+          <label for="email" class="text-sm font-medium text-gray-700">Email</label>
+          <input id="email" type="email" class="input" />
+          <label for="password" class="text-sm font-medium text-gray-700">Mot de passe</label>
+          <input type="password" class="input" />
+          <label for="confirm" class="text-sm font-medium text-gray-700">Confirmer le mot de passe</label>
+          <input id="password" type="password" class="input" />
+          <button class="btn-primary">S'enregistrer</button>
+        </form>
+      </div>
     </div>
   </div>
 </template>
+
