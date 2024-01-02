06c9122581decce9e0c50069614ccb3a3859c670
Sacane <johan.ramaroson@gmail.com>
2024-01-02 23:43:32 +0100

    Fixed month selector in every navigator

--- client/components/monthPicker.vue
+++ client/components/monthPicker.vue
@@ -1,14 +1,9 @@
 <script setup lang="ts">
-import { ref, defineProps, defineEmits} from 'vue';
-const props = defineProps({
-  modelValue: String,
-  onMonthChange: Function,
-})
 const date = useDate()
 
 const selectedMonth = ref('')
 const emits = defineEmits(['update:modelValue']);
-const currentMonth = date.translate(date.monthFromNumber(new Date().getMonth() + 1) as string)
+const currentMonth = ref(date.translate(date.monthFromNumber(new Date().getMonth() + 1) as string))
 
 watchEffect(() => {
   emits('update:modelValue', selectedMonth.value);
@@ -20,36 +15,12 @@ function onMonthChange() {
 </script>
 
 <template>
-  <div class="relative month-picker h10% flex flex-row">
-    <div class="flex justify-center mr2">
-      <label for="monthDropdown" class="text-sm font-medium text-gray-700">Sélectionnez un mois :</label>
-    </div>
-
-    <select
-      v-model="selectedMonth"
-      id="monthDropdown"
-      class="mt-1 block py-2 px-3 border border-gray-300 bg-white rounded-md focus:border-indigo-300 sm:text-sm"
-      @change="onMonthChange"
-    >
-          <!-- Utilisez une option par défaut pour le placeholder -->
-        <option
-        :value="''"
-        :selected="selectedMonth === ''"
-        disabled
-        hidden
-      >
-      {{ currentMonth }}
-      </option>
-      <option
-        v-for="(month, index) in date.months.map(s => date.translate(s))"
-        :key="index"
-        :value="date.months[index]"
-        class="dropdown-option"
-      >
-        {{ month }}
-      </option>
+  <div>
+    <label for="fruit">Choisissez un mois :</label>
+    <select v-model="selectedMonth" id="fruit" @change="onMonthChange">
+      <option :value="currentMonth" disabled hidden>Choisissez un mois</option>
+      <option v-for="(fruit, index) in date.months" :key="index" :value="fruit">{{ fruit }}</option>
     </select>
-
   </div>
 </template>
 
