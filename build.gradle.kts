0fdd67754a54a9f7023cf9b79b3ce70634be32fd
Sacane <johan.ramaroson@gmail.com>
2024-05-27 20:21:54 +0200

    feat - add configuration for sonarqube analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,16 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	kotlin("jvm") version "1.6.21"
 	id("com.github.johnrengelman.shadow") version "7.0.0"
+	id("org.sonarqube") version "5.0.0.4638"
 }
+
+sonar {
+	properties {
+		property("sonar.projectKey", "Sacane_JManager_aa4d0a52-73c4-4b64-a0e2-6f5565902347")
+		property("sonar.projectName", "JManager")
+	}
+}
+
 group = "fr.sacane"
 java.sourceCompatibility = JavaVersion.VERSION_17
 dependencies{
