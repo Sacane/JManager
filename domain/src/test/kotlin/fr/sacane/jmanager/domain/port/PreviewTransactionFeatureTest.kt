af8686c2c5698eeceb1b7ceecf64a718c65b8323
johan <johan.ramaroson@gmail.com>
2024-10-12 16:29:00 +0200

    update some tests

--- domain/src/test/kotlin/fr/sacane/jmanager/domain/port/PreviewTransactionFeatureTest.kt
+++ domain/src/test/kotlin/fr/sacane/jmanager/domain/port/PreviewTransactionFeatureTest.kt
@@ -16,23 +16,5 @@ class PreviewTransactionFeatureTest: FeatureTest() {
     private val accountState: State<AccountByOwner> = FakeFactory.accountState()
     private val previewTransactionFeature: PreviewTransactionFeature = FakeFactory.previewTransactionFeature
 
-    @Nested
-    inner class BookingPreviewTransaction {
 
-        @Test
-        fun `booking a preview transaction should not change the real amount of an account`() {
-            launchWithConnectedUserInstance {
-                previewTransactionFeature.bookPreviewTransaction(UserAccountID(userId, account.id!!, session), "test#0", "01/01/2024".toDate(), 100.toAmount(), true)
-                    .assertSuccess()
-                val actualAccount = accountState.getStates().find { it.userId == userId }?.account?.find { it.id == account.id }
-                val actualAmount = actualAccount?.amount ?: 10.toAmount().negate()
-                val actualPreviewAmount = actualAccount?.previewAmount ?: 0.toAmount()
-
-                assertAll(
-                    {assertEquals(0.toAmount(), actualAmount)},
-                    {assertEquals(100.toAmount(), actualPreviewAmount)}
-                )
-            }
-        }
-    }
 }
