8383fabfaaeb93341cb5b700dcfd69e6edc223c5
Sacane <johan.ramaroson@gmail.com>
2024-10-13 01:09:20 +0200

    ui: externalize account booking dialog to a component

---
+++ client/components/dialog/AccountBookingDialog.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+export interface AccountBookingProps {
+  isVisible: boolean
+  label: string
+  integerpart: string
+  decimalpart: string
+}
+
+const { isVisible, label, integerpart, decimalpart } = defineProps<AccountBookingProps>()
+const emit = defineEmits(['update:isVisible', 'createAccount', 'cancel'])
+
+const accountData = ref({
+  label,
+  integerpart,
+  decimalpart,
+})
+const isVisibleData = ref(isVisible)
+
+watch(() => isVisible, (newValue) => {
+  isVisibleData.value = newValue
+})
+
+function createAccount() {
+  emit('createAccount', accountData.value)
+  closeDialog()
+}
+// Fonction pour fermer le dialog
+function closeDialog() {
+  emit('update:isVisible', false) // Met à jour la visibilité du dialog
+  emit('cancel')
+  isVisibleData.value = false
+}
+</script>
+
+<template>
+  <Dialog
+    v-model:visible="isVisibleData"
+    :reject="closeDialog" class="bg-grey" modal header="Ajouter un nouveau compte"
+  >
+    <div class="mt-6">
+      <div class="flex flex-col gap-3">
+        <label for="label" class="block text-sm font-medium text-gray-700">Libellé du compte</label>
+        <InputText id="label" v-model="accountData.label" type="text" autocomplete="off" />
+      </div>
+
+      <label for="labelAmount" class="block mt-4 text-sm font-medium text-gray-700">Montant</label>
+      <div id="labelAmount" class="flex-row">
+        <InputText v-model="accountData.integerpart" type="string" placeholder="Partie entière" class="" />
+        <InputText v-model="accountData.decimalpart" type="string" placeholder="Partie décimale" maxlength="2" class="" />
+      </div>
+      <Button label="Créer" class="mt-6 w-full bg-purple-600 text-white hover:bg-purple-700" @click="createAccount" />
+      <Button label="Annuler" class="mt-6 w-full bg-purple-600 text-white hover:bg-purple-700" @click="closeDialog" />
+    </div>
+  </Dialog>
+</template>
+
+<style scoped>
+
+</style>
