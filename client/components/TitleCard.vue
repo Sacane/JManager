8c665e4e5f4253ed907aac9c2f7260f4e65dd4f1
johan <johan.ramaroson@gmail.com>
2024-11-18 23:14:23 +0100

    ui: split card into component

---
+++ client/components/TitleCard.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+export interface CardProps {
+  title: string
+  description: string
+  onClick?: () => void
+}
+
+const { title, description, onClick } = defineProps<CardProps>()
+
+function click() {
+  if (onClick) onClick()
+}
+</script>
+
+<template>
+  <div class="card rounded-lg shadow-lg bg-white p-6 mb-8 text-center" @click="click">
+    <h2 class="text-2xl mb-4">
+      {{ title }}
+    </h2>
+    <p class="text-center">
+      {{ description }}
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  transition: transform 0.3s ease;
+}
+.create-container{
+  &:hover{
+    cursor: pointer;
+  }
+}
+
+.card:hover {
+  transform: translateY(-5px);
+}
+</style>
