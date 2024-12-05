b30fdd403c8706d78c7c01c8de5b7be308274285
johan <johan.ramaroson@gmail.com>
2024-12-06 00:43:18 +0100

    refactoring tests

---
+++ domain/src/test/kotlin/fr/sacane/jmanager/domain/InMemoryDatabase.kt
@@ -0,0 +1,143 @@
+package fr.sacane.jmanager.domain
+
+import fr.sacane.jmanager.domain.models.*
+import fr.sacane.jmanager.domain.port.AccountByOwner
+import fr.sacane.jmanager.domain.port.IdUserAccount
+import fr.sacane.jmanager.domain.port.IdUserAccountByTransaction
+
+class InMemoryDatabase {
+    val users = mutableMapOf<UserId, UserWithPassword>()
+
+    private val userByAccount = mutableMapOf<UserId, MutableList<Account>>()
+    private val accountByTransaction = mutableMapOf<Long, MutableList<Transaction>>()
+    private val userByTag = mutableMapOf<UserId, MutableList<Tag>>()
+    private val accountList = mutableListOf<Account>()
+
+    fun addAccount(ownerId: UserId, account: Account) {
+        if(userByAccount[ownerId] == null) {
+            userByAccount[ownerId] = mutableListOf()
+        }
+        userByAccount[ownerId]?.add(account)
+        accountByTransaction[account.id!!] = mutableListOf()
+    }
+    fun removeAccountById(accountId: Long) {
+        userByAccount.forEach {
+            it.value.removeIf { account -> account.id == accountId }
+        }
+        accountByTransaction.remove(accountId)
+    }
+
+    fun upsert(account: Account) {
+        val accountId = account.id
+        userByAccount[account.owner?.id]?.find { it.id == accountId }?.updateFrom(account)
+        accountByTransaction.computeIfAbsent(accountId!!) { mutableListOf() }
+    }
+
+    fun findAccountById(accountId: Long): Account? {
+
+        var targetAccount: Account? = null
+        userByAccount.forEach {
+            val account = it.value.find { acc -> acc.id == accountId }
+            if(account != null) {
+                targetAccount = Account(accountId, account.amount, account.label, accountByTransaction[accountId]!!, initialSold = account.initialSold, previewAmount = account.previewAmount, owner = account.owner)
+            }
+        }
+        return targetAccount
+    }
+
+    fun clearAccounts() {
+        userByAccount.clear()
+        accountByTransaction.clear()
+    }
+
+    private fun accountsWithTransactions(): List<Account> {
+        val accountList = mutableSetOf<Account>()
+        val result = mutableListOf<Account>()
+        userByAccount.forEach { (_, value) -> accountList.addAll(value) }
+
+        accountList.forEach {
+            result.add(
+                Account(it.id, it.amount, it.label, accountByTransaction[it.id]!!, initialSold = it.initialSold, previewAmount = it.previewAmount, owner = it.owner)
+            )
+        }
+        return result
+    }
+
+    fun accountsByOwner(): Collection<AccountByOwner> {
+        return userByAccount.map { AccountByOwner(it.value, it.key) }
+    }
+
+    fun initAccounts(initialState: Collection<AccountByOwner>) {
+        initialState.forEach { accByOwn ->
+            accByOwn.account.forEach {
+                addAccount(accByOwn.userId, it)
+            }
+        }
+    }
+
+    fun addTransaction(userAccountId: IdUserAccount, transaction: Transaction) {
+        val account = userByAccount[userAccountId.userId]?.find { it.id == userAccountId.accountId }
+        account?.addTransaction(transaction)
+        accountByTransaction[account!!.id]?.add(transaction)
+    }
+    fun addMassiveTransaction(collection: Collection<IdUserAccountByTransaction>){
+        collection.forEach { idByTr ->
+            idByTr.transactions.forEach {
+                addTransaction(idByTr.id, it)
+            }
+        }
+    }
+
+    fun upsertTransactions(transactionList: List<Transaction>) {
+
+    }
+    fun removeAllTransactionsById(transactionIds: List<Long>) {
+
+    }
+
+    fun findTransactionById(transactionId: Long): Transaction? {
+        return accountsWithTransactions().flatMap { it.transactions }
+            .find { it.id == transactionId }
+    }
+
+    fun clearUsers() {
+        users.clear()
+    }
+
+    fun initUsers(userCollection: Collection<UserWithPassword>) {
+        users.putAll(userCollection.associateBy { it.user.id })
+    }
+
+    fun findTransactions(): Collection<IdUserAccountByTransaction> {
+        val transactionsResult = mutableMapOf<Pair<IdUserAccount, Long>, Transaction>()
+        userByAccount.forEach { (key, value) ->
+            value.forEach {
+                val id = IdUserAccount(key, it.id!!)
+                accountByTransaction[it.id]!!.forEach { transaction ->
+                    transactionsResult[id to transaction.id!!] = transaction
+                }
+
+            }
+        }
+        val result = mutableMapOf<IdUserAccount, MutableList<Transaction>>()
+        transactionsResult.forEach {
+            if(result[it.key.first] == null) {
+                result[it.key.first] = mutableListOf()
+            }
+            result[it.key.first]?.add(it.value)
+        }
+        return result.map { IdUserAccountByTransaction(it.key, it.value) }
+    }
+
+    fun findAccountByOwnerAndLabel(userId: UserId, accountLabel: String): Account?
+            = accountsWithTransactions().find { it.owner?.id == userId && it.label == accountLabel }
+
+
+    fun clearTransactions() {
+        accountByTransaction.clear()
+    }
+
+    fun saveTransaction(accountId: Long, transaction: Transaction) {
+        accountByTransaction[accountId]?.add(transaction)
+    }
+}
