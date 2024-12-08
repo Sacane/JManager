7131facf1f9a45a8d44274ea95c39df674a036f0
johan <johan.ramaroson@gmail.com>
2024-12-09 00:39:45 +0100

    remove unused variables and imports

--- infra/src/main/kotlin/fr/sacane/jmanager/infrastructure/api/session/Controller.kt
+++ infra/src/main/kotlin/fr/sacane/jmanager/infrastructure/api/session/Controller.kt
@@ -4,13 +4,9 @@ import fr.sacane.jmanager.domain.asTokenUUID
 import fr.sacane.jmanager.domain.hexadoc.Adapter
 import fr.sacane.jmanager.domain.hexadoc.Side
 import fr.sacane.jmanager.domain.port.api.SessionFeature
-import fr.sacane.jmanager.domain.utils.ResultState
-import fr.sacane.jmanager.domain.utils.failure
-import fr.sacane.jmanager.infrastructure.api.InvalidRequestException
 import fr.sacane.jmanager.infrastructure.api.id
 import fr.sacane.jmanager.infrastructure.api.toDTO
 import fr.sacane.jmanager.infrastructure.api.toResponseEntity
-import kotlinx.coroutines.yield
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.logging.Logger
