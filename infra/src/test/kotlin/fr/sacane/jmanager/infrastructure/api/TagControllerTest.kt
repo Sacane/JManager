e2b276bfd8c8d6f3385ba18333a72be37e439830
johan <johan.ramaroson@gmail.com>
2024-12-24 17:03:24 +0100

    fixed pipeline by not delete default tag

--- infra/src/test/kotlin/fr/sacane/jmanager/infrastructure/api/TagControllerTest.kt
+++ infra/src/test/kotlin/fr/sacane/jmanager/infrastructure/api/TagControllerTest.kt
@@ -18,46 +18,46 @@ import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.test.context.TestPropertySource
 
 
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@TestPropertySource(locations = ["classpath:application-test.properties"])
-//class TagControllerTest (
-//    @LocalServerPort val port: Int,
-//    @Autowired val state: TagStateTestAdapter,
-//    @Autowired val objectMapper: ObjectMapper
-//): AuthenticatedUserTest() {
-//
-//    @AfterEach
-//    fun clear() {
-//        state.clear()
-//    }
-//    @Nested
-//    inner class AddPersonalTagEndpointTest {
-//        @Test
-//        fun `Add personal tag endpoint successfully must return 200`() {
-//            val body = UserTagRequest(
-//                user!!.id.value!!,
-//                "Test",
-//                ColorDTO(10, 20, 30)
-//            )
-//            Given {
-//                port(port)
-//                header("Authorization", token)
-//                header("Content-Type", "application/json")
-//                body(objectMapper.writeValueAsString(body))
-//            } When {
-//                post("/api/tag")
-//            } Then {
-//                statusCode(200)
-//                body(
-//                    "label", equalTo("Test"),
-//                    "colorDTO.red", equalTo(10),
-//                    "colorDTO.green", equalTo(20),
-//                    "colorDTO.blue", equalTo(30),
-//                )
-//            }
-//            state.get().find { it.label == "Test" }.apply { assertNotNull(this) }
-//        }
-//    }
-//
-//
-//}
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = ["classpath:application-test.properties"])
+class TagControllerTest (
+    @LocalServerPort val port: Int,
+    @Autowired val state: TagStateTestAdapter,
+    @Autowired val objectMapper: ObjectMapper
+): AuthenticatedUserTest() {
+
+    @AfterEach
+    fun clear() {
+        state.clear()
+    }
+    @Nested
+    inner class AddPersonalTagEndpointTest {
+        @Test
+        fun `Add personal tag endpoint successfully must return 200`() {
+            val body = UserTagRequest(
+                user!!.id.value!!,
+                "Test",
+                ColorDTO(10, 20, 30)
+            )
+            Given {
+                port(port)
+                header("Authorization", token)
+                header("Content-Type", "application/json")
+                body(objectMapper.writeValueAsString(body))
+            } When {
+                post("/api/tag")
+            } Then {
+                statusCode(200)
+                body(
+                    "label", equalTo("Test"),
+                    "colorDTO.red", equalTo(10),
+                    "colorDTO.green", equalTo(20),
+                    "colorDTO.blue", equalTo(30),
+                )
+            }
+            state.get().find { it.label == "Test" }.apply { assertNotNull(this) }
+        }
+    }
+
+
+}
