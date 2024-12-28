6e1db5562adaf62d5912bfc955108038b494cd85
Sacane <johan.ramaroson@gmail.com>
2024-12-28 23:31:47 +0100

    test: add and fixed creation logout and login tests

--- infra/src/test/kotlin/fr/sacane/jmanager/infrastructure/api/SessionControllerTest.kt
+++ infra/src/test/kotlin/fr/sacane/jmanager/infrastructure/api/SessionControllerTest.kt
@@ -1,12 +1,13 @@
 package fr.sacane.jmanager.infrastructure.api
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import fr.sacane.jmanager.infrastructure.api.session.RegisteredUserDTO
 import fr.sacane.jmanager.infrastructure.api.session.UserPasswordDTO
-import io.restassured.RestAssured
+import fr.sacane.jmanager.infrastructure.spi.repositories.UserPostgresRepository
 import io.restassured.module.kotlin.extensions.Given
 import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
-import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -19,15 +20,15 @@ import org.springframework.test.context.TestPropertySource
 @TestPropertySource(locations = ["classpath:application-test.properties"])
 class SessionControllerTest(
     @LocalServerPort private val port: Int,
-    @Autowired val objectMapper: ObjectMapper
+    @Autowired val objectMapper: ObjectMapper,
+    @Autowired val userRepository: UserPostgresRepository
 ): AuthenticatedUserTest() {
-    companion object {
-        @BeforeAll
-        @JvmStatic
-        fun setup() {
-            RestAssured.enableLoggingOfRequestAndResponseIfValidationFails()
-        }
+
+    @AfterEach
+    fun clear() {
+        userRepository.deleteAll()
     }
+
     @Nested
     inner class LoginEndpointTest {
         @Test
@@ -35,7 +36,7 @@ class SessionControllerTest(
             Given {
                 port(port)
                 header("Content-Type", "application/json")
-                body(objectMapper.writeValueAsString(UserPasswordDTO("test", "test")))
+                body(objectMapper.writeValueAsString(UserPasswordDTO(user!!.username, "test")))
             } When {
                 post("/api/user/auth")
             } Then {
@@ -55,6 +56,33 @@ class SessionControllerTest(
                 post("/api/user/logout/${user!!.id.value}")
             } Then {
                 statusCode(200)
+            }
+        }
+    }
+    @Nested
+    inner class CreateUserEndpointTest {
+        @Test
+        fun `Create a user must return 200`() {
+            Given {
+                port(port)
+                header("Content-Type", "application/json")
+                body(objectMapper.writeValueAsString(RegisteredUserDTO("test2", "test2", "test2")))
+            } When {
+                post("/api/user/create")
+            } Then {
+                statusCode(200)
+            }
+        }
+        @Test
+        fun `Create a user that has not the same password and confirm password must return 401`() {
+            Given {
+                port(port)
+                header("Content-Type", "application/json")
+                body(objectMapper.writeValueAsString(RegisteredUserDTO("test", "test", "test2")))
+            } When {
+                post("/api/user/create")
+            } Then {
+                statusCode(401)
             }
         }
     }
