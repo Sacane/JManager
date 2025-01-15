48c53e36495142526ae84bba9d6560c058d05d99
johan <johan.ramaroson@gmail.com>
2025-01-16 00:42:58 +0100

    test: add test for transaction adapter persist method

--- infra/src/test/kotlin/fr/sacane/jmanager/infrastructure/api/AuthenticatedUserTest.kt
+++ infra/src/test/kotlin/fr/sacane/jmanager/infrastructure/api/AuthenticatedUserTest.kt
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+
 abstract class AuthenticatedUserTest {
     lateinit var token: String
     var user: User? = null
