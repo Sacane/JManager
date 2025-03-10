bc844c96c4f772ad84c4c4563a59a0ad9befb8d9
johan <johan.ramaroson@gmail.com>
2025-03-10 01:10:51 +0100

    refactor: update layout and styling for sidebar and related components

--- client/components/sidebar.vue
+++ client/components/sidebar.vue
@@ -1,60 +1,73 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import useAuth from '../composables/useAuth'
 import 'primeicons/primeicons.css'
 
 const { isAuthenticated, logout, user } = useAuth()
+const isSidebarOpen = ref(true)
 </script>
 
 <template>
-  <div class="hidden lg:flex flex-col border-r w-12% h-screen text-center justify-between content position-fixed">
-    <div class="flex flex-col">
-      <NuxtLink to="/" class="title decoration-none">
-        <img src="@/public/favicon.ico" alt="icon" class="w-35px"> JManager
-      </NuxtLink>
-      <div class="mt5 flex flex-col flex-gap-4">
-        <NuxtLink
-          to="/" class="hover:bg-gray-200 py-2 px-4 decoration-none section flex flex-row overflow-hidden gap-3" active-class="bg-primary-color-white"
-        >
-          <i class="pi pi-home text-25px" />
-          Accueil
+  <div>
+    <div :class="{ 'sidebar-open': isSidebarOpen, 'sidebar-closed': !isSidebarOpen }" class="sidebar lg:flex flex-col border-r w-full lg:w-80 h-screen text-center justify-between content fixed lg:relative">
+      <button v-if="isSidebarOpen" :class="{ open: isSidebarOpen }" class="lg:hidden p-2" @click="isSidebarOpen = !isSidebarOpen">
+        <i :class="isSidebarOpen ? 'pi pi-times' : 'pi pi-bars'" class="text-2xl" />
+      </button>
+      <div class="flex flex-col">
+        <NuxtLink to="/" class="title decoration-none">
+          <img src="@/public/favicon.ico" alt="icon" class="w-15 lg:w-10"> Jmanager
         </NuxtLink>
-        <NuxtLink v-if="isAuthenticated" to="/dashboard" class="hover:bg-gray-200 py-2 px-4 decoration-none section flex flex-row overflow-hidden gap-3" active-class="bg-primary-color-white">
-          <i class="pi pi-tag text-25px" /> Tableau de bord
-        </NuxtLink>
-        <NuxtLink v-if="isAuthenticated" to="/account" class="hover:bg-gray-200 py-2 px-4 decoration-none section flex flex-row overflow-hidden gap-3" active-class="bg-primary-color-white">
-          <i class="pi pi-wallet text-25px" /> Mes livrets
-        </NuxtLink>
-        <NuxtLink v-if="isAuthenticated" to="/tag" class="hover:bg-gray-200 py-2 px-4 decoration-none section flex flex-row overflow-hidden gap-3" active-class="bg-primary-color-white">
-          <i class="pi pi-tag text-25px" /> Mes tags
-        </NuxtLink>
+        <div class="mt-5 flex flex-col gap-4">
+          <NuxtLink
+            to="/" class="hover:bg-gray-200 py-2 px-4 decoration-none section flex flex-row overflow-hidden gap-3" active-class="bg-primary-color-white"
+          >
+            <i class="pi pi-home text-2xl lg:text-3xl" />
+            Accueil
+          </NuxtLink>
+          <NuxtLink v-if="isAuthenticated" to="/dashboard" class="hover:bg-gray-200 py-2 px-4 decoration-none section flex flex-row overflow-hidden gap-3" active-class="bg-primary-color-white">
+            <i class="pi pi-tag text-2xl lg:text-3xl" /> Tableau de bord
+          </NuxtLink>
+          <NuxtLink v-if="isAuthenticated" to="/account" class="hover:bg-gray-200 py-2 px-4 decoration-none section flex flex-row overflow-hidden gap-3" active-class="bg-primary-color-white">
+            <i class="pi pi-wallet text-2xl lg:text-3xl" /> Mes livrets
+          </NuxtLink>
+          <NuxtLink v-if="isAuthenticated" to="/tag" class="hover:bg-gray-200 py-2 px-4 decoration-none section flex flex-row overflow-hidden gap-3" active-class="bg-primary-color-white">
+            <i class="pi pi-tag text-2xl lg:text-3xl" /> Mes tags
+          </NuxtLink>
+        </div>
       </div>
-    </div>
 
-    <div class="m2">
-      <p class="mb5">
-        Connecté en tant que <b>{{ user?.username }}</b>
-      </p>
-      <div class="flex-row text-15px flex justify-center">
-        <NuxtLink v-if="isAuthenticated" class="icon-btn mb2px" @click="logout()">
-          <i class="pi pi-sign-out" />
-          Se deconnecter
-        </NuxtLink>
-        <NuxtLink v-else to="/login" class="icon-btn">
-          <div i="tabler-login" />
-          Se connecter
-        </NuxtLink>
+      <div class="m-2">
+        <p class="mb-5">
+          Connecté en tant que <b>{{ user?.username }}</b>
+        </p>
+        <div class="flex-row text-lg flex justify-center">
+          <NuxtLink v-if="isAuthenticated" class="icon-btn mb-2" @click="logout()">
+            <i class="pi pi-sign-out" />
+            Se deconnecter
+          </NuxtLink>
+          <NuxtLink v-else to="/login" class="icon-btn">
+            <div i="tabler-login" />
+            Se connecter
+          </NuxtLink>
+        </div>
       </div>
     </div>
+    <button v-if="!isSidebarOpen" class="lg:hidden p-2" @click="isSidebarOpen = !isSidebarOpen">
+      <i :class="isSidebarOpen ? 'pi pi-times' : 'pi pi-bars'" class="text-2xl" />
+    </button>
   </div>
 </template>
 
 <style scoped lang="scss">
-.content{
+.content {
   background-color: var(--primary);
   border-right: 1px solid #fff;
-  @media (max-width: 770px) {
-    width: 20%;
-  }
+}
+.open {
+  z-index: 2000;
+}
+.closed {
+  width: 0;
 }
 .title {
   color: #fff;
@@ -64,38 +77,29 @@ const { isAuthenticated, logout, user } = useAuth()
 
 .section {
   color: #fff;
-  font-size: 23px;
+  font-size: 1.5rem;
   background-color: var(--primary-background);
 
-  &:hover{
+  &:hover {
     opacity: 0.6;
   }
-  @media (min-width: 1201px) {
-    i {
-      align-self: flex-start;
-      justify-self: flex-start;
-    }
-  }
 }
 
-@media screen and (min-width: 1201px) {
-  .title{
-    font-size: 2rem;
-  }
+.sidebar {
+  transition: transform 0.3s ease-in-out;
+  z-index: 1000;
+  width: 100%;
 }
 
-@media screen and (max-width: 768px) {
-  .title{
-    font-size: 1rem;
-  }
+.sidebar-open {
+  transform: translateX(0);
 }
 
-@media screen and (min-width: 769px) and (max-width: 1200px) {
-  .title{
-    font-size: 1.5rem;
-  }
+.sidebar-closed {
+  transform: translateX(-200%);
 }
-.bg-primary-color-white{
+
+.bg-primary-color-white {
   background-color: white;
   color: var(--primary);
 }
--- client/layouts/sidebar-layout.vue
+++ client/layouts/sidebar-layout.vue
@@ -6,11 +6,17 @@
   <div class="flex flex-row w-screen h-screen">
     <!-- ------------------------------ -->
     <Sidebar class="sidebar" />
-    <div class="lg:(w-full h-screen flex flex-col ml-13%)">
+    <div class="content lg:(w-full h-screen flex flex-col)">
       <slot />
     </div>
   </div>
 </template>
 
 <style scoped>
+.sidebar {
+  width: 15%;
+}
+.content {
+  width: 85%;
+}
 </style>
