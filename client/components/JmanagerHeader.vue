e2acb1121a9e262f8ee9434f6bffa5b4c042fc32
johan <johan.ramaroson@gmail.com>
2025-03-14 18:13:52 +0100

    refactor: add JmanagerHeader component for user display and enhance sidebar layout

---
+++ client/components/JmanagerHeader.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const { user, logout } = useAuth()
+
+const initial = computed(() => user.value?.username.charAt(0).toUpperCase())
+</script>
+
+<template>
+  <div class="header-content">
+    <div class="circle">
+      {{ initial }}
+    </div>
+    <div>
+      {{ user.username }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.header-content {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  height: 100%;
+  gap: 10px;
+}
+.circle {
+  width: 45px;
+  height: 45px;
+  border-radius: 50%;
+  background-color: var(--primary);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-size: 30px;
+  font-weight: bold;
+}
+</style>
