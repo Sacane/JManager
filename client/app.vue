24547295abb9cc2911e83ec66c6d87c9ea9c4f61
johan <johan.ramaroson@gmail.com>
2025-03-25 11:35:52 +0100

    feat: add French localization options for PrimeVue components and improve calendar configuration

--- client/app.vue
+++ client/app.vue
@@ -2,14 +2,13 @@
 // switch to preferred locale on load
 import 'primeicons/primeicons.css'
 
-/*
 const router = useRouter()
 const switchLocalePath = useSwitchLocalePath()
 const locale = useLocale()
 
 tryOnBeforeMount(() => {
   router.replace(switchLocalePath(locale.value))
-}) */
+})
 </script>
 
 <template>
