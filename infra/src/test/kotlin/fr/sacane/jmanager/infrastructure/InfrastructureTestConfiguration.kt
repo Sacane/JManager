71d78d3b31d6a928f50781d2773f0bb18f0a3173
johan <johan.ramaroson@gmail.com>
2025-03-31 11:33:10 +0200

    feat: add custom serializer for LocalDate in test configuration

--- infra/src/test/kotlin/fr/sacane/jmanager/infrastructure/InfrastructureTestConfiguration.kt
+++ infra/src/test/kotlin/fr/sacane/jmanager/infrastructure/InfrastructureTestConfiguration.kt
@@ -1,10 +1,17 @@
 package fr.sacane.jmanager.infrastructure
 
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.JsonSerializer
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.module.SimpleModule
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Profile
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import javax.sql.DataSource
 
 @TestConfiguration
@@ -14,4 +21,27 @@ class InfrastructureTestConfiguration {
     fun dataSource(): DataSource = EmbeddedDatabaseBuilder()
         .setType(EmbeddedDatabaseType.H2)
         .build()
+}
+
+class TestLocalDateSerializer : JsonSerializer<LocalDate>() {
+    private val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+
+    override fun serialize(value: LocalDate, gen: JsonGenerator, serializers: SerializerProvider) {
+        gen.writeString(value.format(formatter))
+    }
+}
+
+@TestConfiguration
+class Config {
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        val module = SimpleModule()
+        module.addSerializer(LocalDate::class.java,
+            TestLocalDateSerializer()
+        )
+
+        return ObjectMapper()
+            .findAndRegisterModules()
+            .registerModule(module)
+    }
 }
