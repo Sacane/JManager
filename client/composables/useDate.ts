a02897a4d1f848bd36e6ff3416938cc3a2136245
johan <johan.ramaroson@gmail.com>
2025-04-26 13:00:47 +0200

    fix(): update date format in LocalDateSerializer to ISO standard and clean up unused styles in TransactionCreationDialog

--- client/composables/useDate.ts
+++ client/composables/useDate.ts
@@ -79,7 +79,7 @@ export default function useDate() {
     const month = String(date.getMonth() + 1).padStart(2, '0')
     const year = date.getFullYear()
 
-    return `${day}-${month}-${year}`
+    return `${year}-${month}-${day}`
   }
 
   return { months, translate, monthFromNumber, dateFromString, formattedDateString, englishMonth }
