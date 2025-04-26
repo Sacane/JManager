d88e8c7222fe883ffd5d0cdb0771738394d33380
johan <johan.ramaroson@gmail.com>
2025-04-26 13:35:18 +0200

    fix(): update date handling in TransactionResult and related classes to use String format

--- infra/src/main/kotlin/fr/sacane/jmanager/infrastructure/configuration/DateFormattedTest.kt
+++
@@ -1,9 +0,0 @@
-package fr.sacane.jmanager.infrastructure.configuration
-
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-
-fun formattedDate(date: LocalDate): LocalDate {
-    val formatter = DateTimeFormatter.ofPattern("YYYY-MM-dd")
-    return LocalDate.parse(date.format(formatter), formatter)
-}
