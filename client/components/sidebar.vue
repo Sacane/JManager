7dd866238a53d0ac3ab30d083592c99a870e33ff
johan <johan.ramaroson@gmail.com>
2025-05-05 13:19:56 +0200

    refactor(): enhance table header styling for better visibility and user experience

--- client/components/sidebar.vue
+++ client/components/sidebar.vue
@@ -28,7 +28,7 @@ function closeOnNavigateIfMobile() {
         </NuxtLink>
         <div class="mt-5 flex flex-col gap-4">
           <NuxtLink v-if="isAuthenticated" to="/dashboard" class="hover:bg-gray-200 py-2 px-4 decoration-none section flex flex-row overflow-hidden gap-3" active-class="bg-primary-color-white" @click="closeOnNavigateIfMobile()">
-            <i class="pi pi-tag text-2xl" /> Tableau de bord
+            <i class="pi pi-home text-2xl" /> Tableau de bord
           </NuxtLink>
           <NuxtLink v-if="isAuthenticated" to="/account" class="hover:bg-gray-200 py-2 px-4 decoration-none section flex flex-row overflow-hidden gap-3" active-class="bg-primary-color-white" @click="closeOnNavigateIfMobile()">
             <i class="pi pi-wallet text-2xl" /> Mes livrets
