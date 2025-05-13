cf7531e34c801c1348114e9df5c39ef17f587cb6
johan <johan.ramaroson@gmail.com>
2025-05-13 02:16:48 +0200

    feat(transaction): add in-memory and JPA repository implementations for regular transactions

--- domain/src/main/kotlin/fr/sacane/jmanager/domain/port/spi/RegularTransactionRepository.kt
+++ domain/src/main/kotlin/fr/sacane/jmanager/domain/port/spi/RegularTransactionRepository.kt
@@ -4,7 +4,4 @@ import fr.sacane.jmanager.domain.models.RegularTransaction
 
 interface RegularTransactionRepository {
     fun saveRegularTransaction(regularTransaction: RegularTransaction): RegularTransaction
-    fun findRegularTransactionById(id: String): RegularTransaction
-    fun deleteRegularTransactionById(id: String): Nothing
-    fun findAllRegularTransactions(): List<RegularTransaction>
 }
---
+++ infra/src/main/kotlin/fr/sacane/jmanager/infrastructure/spi/adapters/DataJpaRegularTransactionRepositoryAdapter.kt
@@ -0,0 +1,12 @@
+package fr.sacane.jmanager.infrastructure.spi.adapters
+
+import fr.sacane.jmanager.domain.models.RegularTransaction
+import fr.sacane.jmanager.domain.port.spi.RegularTransactionRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+class DataJpaRegularTransactionRepositoryAdapter: RegularTransactionRepository {
+    override fun saveRegularTransaction(regularTransaction: RegularTransaction): RegularTransaction {
+        TODO("Not yet implemented")
+    }
+}
