c132fc4903646a326d5165e409a173c51f7f0830
johan <johan.ramaroson@gmail.com>
2025-11-05 17:37:46 +0100

    refactor(Profile.vue): enhance user info display with improved styling and layout

--- client/components/Profile.vue
+++ client/components/Profile.vue
@@ -39,11 +39,11 @@ onBeforeUnmount(() => {
       <div class="w-12.5 h-12.5 rounded-full bg-white flex items-center justify-center text-purple-600 text-3xl font-bold cursor-pointer" @click="toggleDropdown">
         {{ username.charAt(0) }}
       </div>
-      <div>
-        <p class="m-0 text-white">
+      <div class="flex flex-col gap-1">
+        <p class="m-0 text-white font-medium">
           {{ username }}
         </p>
-        <p class="text-xs text-white/80 mt--3 m-0">
+        <p class="text-xs text-white/80 m-0">
           {{ isAdmin ? 'Administrateur' : 'Utilisateur' }}
         </p>
       </div>
